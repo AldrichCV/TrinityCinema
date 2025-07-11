89e3f6089a8d7c93df28099440bb6b4564481cc9
Cess <Cess@CESS>
2025-07-11 11:01:44 +0800

    Balo lang unsa ni

--- TrinityCinema/Views/Admin/TheaterInfo.Designer.cs
+++ TrinityCinema/Views/Admin/TheaterInfo.Designer.cs
@@ -28,68 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TheaterInfo));
             this.teName = new DevExpress.XtraEditors.TextEdit();
             this.teSeats = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.btnSubmit = new DevExpress.XtraEditors.SimpleButton();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
             ((System.ComponentModel.ISupportInitialize)(this.teName.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.teSeats.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // teName
             // 
-            this.teName.Location = new System.Drawing.Point(28, 34);
+            this.teName.Location = new System.Drawing.Point(32, 51);
+            this.teName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.teName.Name = "teName";
-            this.teName.Size = new System.Drawing.Size(231, 28);
+            this.teName.Size = new System.Drawing.Size(270, 34);
             this.teName.TabIndex = 1;
             // 
             // teSeats
             // 
-            this.teSeats.Location = new System.Drawing.Point(28, 100);
+            this.teSeats.Location = new System.Drawing.Point(33, 123);
+            this.teSeats.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.teSeats.Name = "teSeats";
-            this.teSeats.Size = new System.Drawing.Size(231, 28);
+            this.teSeats.Size = new System.Drawing.Size(270, 34);
             this.teSeats.TabIndex = 2;
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(27, 22);
+            this.labelControl1.Location = new System.Drawing.Point(33, 27);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(68, 13);
+            this.labelControl1.Size = new System.Drawing.Size(82, 16);
             this.labelControl1.TabIndex = 4;
             this.labelControl1.Text = "Theater Name";
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(28, 81);
+            this.labelControl2.Location = new System.Drawing.Point(33, 100);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(67, 13);
+            this.labelControl2.Size = new System.Drawing.Size(78, 16);
             this.labelControl2.TabIndex = 5;
             this.labelControl2.Text = "Seat Capacity";
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(153, 163);
+            this.btnSubmit.Location = new System.Drawing.Point(179, 194);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(106, 48);
+            this.btnSubmit.Size = new System.Drawing.Size(124, 38);
             this.btnSubmit.TabIndex = 6;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
+            this.pictureEdit1.Location = new System.Drawing.Point(-24, -11);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pictureEdit1.Size = new System.Drawing.Size(410, 305);
+            this.pictureEdit1.TabIndex = 7;
+            // 
             // TheaterInfo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(293, 223);
+            this.ClientSize = new System.Drawing.Size(342, 274);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.teSeats);
             this.Controls.Add(this.teName);
+            this.Controls.Add(this.pictureEdit1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TheaterInfo";
             this.Text = "TheaterInfo";
             ((System.ComponentModel.ISupportInitialize)(this.teName.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.teSeats.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,5 +123,6 @@
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.SimpleButton btnSubmit;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
     }
 }
